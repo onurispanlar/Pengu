8ae8cd60445b7ae75df7f3efb92444136618b60c
sengitu <ugurcansengit@ugurcansengit.koding.io>
2016-02-21 09:15:11 -0600

    Resolved falan

--- Applications/YuruyenUcak/server.js
+++ Applications/YuruyenUcak/server.js
@@ -78,101 +78,109 @@ router.route("/logout")
   });  
 
 router.route("/articles")
-  .get(function(req, res) {
-    res.json({
-      "error": true,
-      "message": "Please specify identifier for articles"
+        .get(function(req, res) {
+          res.json({
+              "error": true,
+              "message": "Please specify identifier for articles"
+            });
+        })
+    .post(function(req, res) {
+        userController.getUserByUsername(req.body.username,
+            function getUsernameSuccessCallback(user) {
+                var params = {};
+                
+                params.title = req.body.title;
+                params.authorId = user._id;
+                params.publishedDate = req.body.publishedDate;
+                params.updatedDate = req.body.updatedDate;
+                params.pageCount = req.body.pageCount;
+                params.language = req.body.language;
+                params.tags = req.body.tags;
+                params.content = req.body.content;
+                
+                articleController.createArticle(params,
+                    function createArticleSuccessCallback() {
+                        res.json({
+                            "error": false,
+                            "message": "Article added successfully"
+                        });
+                    },
+                    function createArticleFailureCallback(error) {
+                        res.json({
+                            "error": true,
+                            "message": "Failed to create article"
+                        });
+                    });
+                
+            },
+        function getUsernameFailureCallback() {
+            res.json({
+                "error": true,
+                "message": "Failed to fetch given username"
+            });
+        });
     });
-  })
-  .post(function(req, res) {
-    var params = {};
 
-    userController.getUserByUsername(req.body.username,
-        function getUsernameSuccessCallback(user) {
+    router.route("/articles/:id")
+        .get(function(req, res) {
+            articleController.getArticleById(req.params.id,
+                function getArticleByIdSuccessCallback(data) {
+                    res.json({
+                      "error": false,
+                      "message": data
+                    });
+                },
+                function getArticleByIdFailureCallback() {
+                    res.json({
+                        "error": true,
+                        "message": "Failed to fetch given article"
+                    });
+                });
+        })
+        .put(function(req, res) {
+            var params = {};
+            
             params.title = req.body.title;
-            params.author_id = user.id;
-            params.pulished_date = req.body.pulished_date;
-            params.created_date = req.body.created_date;
-            params.updated_date = req.body.updated_date;
-            params.page_count = req.body.page_count;
+            params.authorId = params.authorId;
+            params.pulishedDate = req.body.pulishedDate;
+            params.updatedDate = req.body.updatedDate;
+            params.pageCount = req.body.pageCount;
             params.language = req.body.language;
             params.tags = req.body.tags;
             params.content = req.body.content;
             
-            articleController.createArticle(params,
-                function createArticleSuccessCallback() {
+            articleController.updateArticle(
+                params,
+                function updateArticleSuccessCallback(data) {
                     res.json({
-                        "error": false,
-                        "message": "Article added successfully"
+                      "error": false,
+                      "message": "Data is updated for " + req.params.id
                     });
-                },
-                function createArticleFailureCallback(error) {
+                }, 
+                function updateArticleFailureCallback(error) {
                     res.json({
-                        "error": true,
-                        "message": "Failed to create article"
+                      "error": true,
+                      "message": "Failed to update data"
                     });
                 });
-            
-        },
-    function getUsernameFailureCallback() {
-        res.json({
-            "error": true,
-            "message": "Failed to fetch given username"
-        });
-    });
-});
-
-router.route("/articles/:id")
-    .get(function(req, res) {
-        articleController.getArticleById(req.params.id,
-            function getArticleByIdSuccessCallback(data) {
-                res.json({
-                  "error": false,
-                  "message": data
-                });
-            },
-            function getArticleByIdFailureCallback() {
-                res.json({
-                    "error": true,
-                    "message": "Failed to fetch given article"
-                });
-            });
     })
-    .put(function(req, res) {
-        params.title = req.body.title;
-        params.author_id = params.author_id;
-        params.pulished_date = req.body.pulished_date;
-        params.created_date = req.body.created_date;
-        params.updated_date = req.body.updated_date;
-        params.page_count = req.body.page_count;
-        params.language = req.body.language;
-        params.tags = req.body.tags;
-        params.content = req.body.content;
-        
-        articleController.updateArticle(
-            params,
-            function updateArticleSuccessCallback(data) {
-                res.json({
-                  "error": false,
-                  "message": "Data is updated for " + req.params.id
-                });
-            }, 
-            function updateArticleFailureCallback(error) {
-                res.json({
-                  "error": true,
-                  "message": "Failed to update data"
-                });
+    .delete(function(req, res) {
+        articleController.deleteArticle(req.params.id,
+            function deleteArticleSuccessCallback(response) {
+                res.json(response);
+            },
+            function deleteArticleFailureCallback(response) {
+                res.json(response);
             });
-})
-.delete(function(req, res) {
-    articleController.deleteArticle(req.params.id,
-        function deleteArticleSuccessCallback(response) {
-            res.json(response);
-        },
-        function deleteArticleFailureCallback(response) {
-            res.json(response);
+    });
+    
+    router.route("/votes")
+        .get(function(req, res) {
+            res.json({
+                "error": true,
+                "message": "Please specify identifier for articles"
+            });
         });
-});
 
 router.route("/books")
   .get(function(req, res) {
