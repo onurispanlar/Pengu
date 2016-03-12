0c4102cae635ed5f8a6ea9c8fe8d207e70287875
ispanlar <ispanlar@netas.com.tr>
2016-03-13 00:44:02 +0200

    add author.html & add dummy data to authorController.js

--- Web/src/js/author/authorController.js
+++ Web/src/js/author/authorController.js
@@ -1,3 +1,13 @@
 angular.module('author', []).controller('authorController', ['$scope', function($scope) {
-
+  $scope.authorList = [{
+    name: "onur ispanlar",
+    likeCount: 89857,
+    viewCount: 754,
+    chapterCount: 3
+  }, {
+    name: "ugurcan sengit",
+    likeCount: 2547,
+    viewCount: 8569,
+    chapterCount: 9
+  }];
 }]);
