d92007808352b80ec24368bf8e4aa0e90be269c8
ispanlar <ispanlar@netas.com.tr>
2016-03-13 00:38:56 +0200

    add book.html & add dummy data to bookController.js

--- Web/src/js/book/bookController.js
+++ Web/src/js/book/bookController.js
@@ -1,4 +1,21 @@
 angular.module('book', []).controller('bookController', ['$scope', function($scope) {
-
+  $scope.bookList = [{
+    title: "TURKULER",
+    author: "Turkulerimiz",
+    likeCount: 325,
+    viewCount: 982348,
+    chapterCount: 1
+  }, {
+    title: "GOD",
+    author: "Is An Astronaut",
+    likeCount: 385668,
+    viewCount: 466,
+    chapterCount: 52
+  }, {
+    title: "EHEHEHEHE",
+    author: "Mehehehehe",
+    likeCount: 3456347,
+    viewCount: 5333,
+    chapterCount: 4
+  }];
 }]);
-
