aa39227d77afdfd01242a6bb43685c541e10f03a
ispanlar <ispanlar@netas.com.tr>
2016-03-13 18:43:29 +0200

    add dummy data

--- Web/src/js/article/articleController.js
+++ Web/src/js/article/articleController.js
@@ -1,21 +1,32 @@
 angular.module('article', []).controller('articleController', ['$scope', function($scope) {
+    $scope.article = {
+        id: 99,
+        title: "BIRINCI OSMAN",
+        author: "Osman",
+        likeCount: 2,
+        viewCount: 0
+    };
     $scope.articleList = [{
+        id: 99,
         title: "BIRINCI OSMAN",
         author: "Osman",
         likeCount: 2,
         viewCount: 0
     }, {
+        id: 949,
         title: "EN BUYK OSMAN",
         author: "Kucuk Osman",
         likeCount: 224124,
         viewCount: 8349
     }, {
+        id: 56858,
         title: "NABER ;)",
         author: "Buyuk Osman",
         likeCount: 325,
         viewCount: 76,
         type: "Documentation"
     }, {
+        id: 57,
         title: "Ben kocaman isimli bir chapter'ım, Ben kocaman isimli bir chapter'ım, Ben kocaman isimli bir chapter'ım, Ben kocaman isimli bir chapter'ım, Ben kocaman isimli bir chapter'ım",
         author: "Jean Paul Osman",
         likeCount: 325,
